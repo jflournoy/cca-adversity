e139834d53f1eb77febefc4fc55e3b33cf751600
John Flournoy <jflournoy@holy2a06208.rc.fas.harvard.edu>
2019-02-19 18:55:20 +0000

    some more explanation

--- test_pma.R
+++ test_pma.R
@@ -26,9 +26,35 @@ somedata <- make_fake_data(300, 40, 19*18/2)
 X <- somedata$X # pretend each is a self report item
 Z <- somedata$Z # pretend each is a connectivity value
 
-# check out the help file: ?PMA::CCA.permute
+#check out the help file: 
+#?PMA::CCA
+#?PMA::CCA.permute
 
-#choose tuning parameter based on the first canonical variates
+#Choose tuning parameter based on the first canonical variates
+#Basically what's going on here is this:
+#For each pair of tuning parameters (the penalties), the rows of X are shuffled
+#(so they no longer correspond to to the rows of Y -- this is makes the null
+#hypothesis true). A CCA is computed and the correlation between the canonical
+#variates is found, c*_i for each of i permutations. The correlation of the
+#canonical variates on the unpermuted data is also found, called c. All c*, and
+#c, are transformed using fisherz transformation. Then a Z statistic is computed
+#that looks at how extreme the original data correlation is in reference to the
+#distribution of c* computed on the shuffled/permuted data. We want tuning
+#parameters that maximize the correlation in the real data relative to the null.
+#The z statistic is just:
+# (Fisher(c) - mean(Fisher(c*))) / sd(Fisher(c*))
+#
+
 system.time(acca <- CCA.permute(X, Z, typex = 'standard', typez = 'standard', nperms = 100))
 print(acca)
 plot(acca)
+
+acca2 <- CCA(X, Z, typex = 'standard', typez = 'standard', 
+             penaltyx = acca$bestpenaltyx,
+             penaltyz = acca$bestpenaltyz,
+             K = 5)
+
+print(acca2)
+
+#Show out-of-sample prediction performance
+#Calculate p-value of this procedure
