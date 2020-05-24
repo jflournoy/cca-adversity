4a1171b8a7d6e8983ec6d1baff65dec7f97bf2da
John Flournoy <john_flournoy@fas.harvard.edu>
2020-05-24 14:24:37 +0000

    command line flexibility

--- CCA.R
+++ CCA.R
@@ -1,18 +1,46 @@
-# CCA Analysis Questionnaire Coding
+# CCA Analysis
 # Katherine Grisanzio & John Flournoy
 
-NCPU = as.numeric(Sys.getenv('SLURM_CPUS_ON_NODE')) #How many CPUS we have to run this chunk
-TASK_ID = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID')) #This chunk's number
-MAX_TASKS = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_MAX')) #How many total chunks this has been split up into
-#MAX_TASKS=3
-#TASK_ID=2
-SELECTION = 'drysdale'
+library(argparse)
+library(PMA)
+library(readr)
+
+parser <- ArgumentParser(description='Run a simulation study using permediatr')
+parser$add_argument('--nopermute', action = 'store_true', help = 'Run the CCA on the unpermuted observed data and save the result. Do not run any permutations (this CCA will not be computed on unpermuted data when running permutations).')
+parser$add_argument('--selectfun', type="character", required = TRUE,
+                    help = 'Selection function to use for data reduction prior to CCA. The character string given here will be postpended to the string `select_features_`. To specify the function `select_features_drysdale` use "--selectfun drysdale".')
+parser$add_argument('--maxchunks', type="integer",
+                    help = 'Number of total chunks to split permutation index into.')
+parser$add_argument('--chunkid', type="integer",
+                    help = 'Index of the chunk of permutations to be run after the permutation index has been split into the number of chunks specified by "--maxchunks"')
+parser$add_argument('--mc.cores', type="integer", default = 1,
+                    help = 'Number of cores to parallelize over.')
+parser$add_argument('--datadir', type="character",default = 'data', 
+                    help = 'Directory where data and permutation index are stored, and where output is saved.')
+parser$add_argument('--permfile', type="character",default = 'permute_index.rds',
+                    help = 'RDS file containing permutation matrix')
+parser$add_argument('--innerperms', type="integer",default = 1000,
+                    help = 'Number of permutations for choosing penalty.')
+
+args <- parser$parse_args(c('--nopermute',
+                            '--selectfun', 'drysdale2',
+                            '--maxchunks', '3',
+                            '--chunkid', '1'))
+NCPU = args$mc.cores
+if(!is.na(as.numeric(Sys.getenv('SLURM_CPUS_ON_NODE'))) & 
+   NCPU < as.numeric(Sys.getenv('SLURM_CPUS_ON_NODE'))){
+  warning('mc.cores option is set to fewer than the available cores. Processing will be inefficient.')
+}
+CHUNK_ID = args$chunkid
+MAX_TASKS = args$maxchunks
+SELECTION = args$selectfun
+NPERMS = args$innerperms
 
 if(!file.exists('data/permute_index.rds')){
   stop('Please generate the permutations using `generate_permutations.R`.')
 } else {
   permutations <- readRDS('data/permute_index.rds')
-  if(any(is.na(c(NCPU, TASK_ID, MAX_TASKS)))){
+  if(any(is.na(c(NCPU, CHUNK_ID, MAX_TASKS)))){
     message('Not running on NCF, setting small values for test purposes')
     NCPU = 10
     fname = file.path('data', 'test.rds')
@@ -20,10 +48,10 @@ if(!file.exists('data/permute_index.rds')){
   } else {
     message('Running on NCF.
 Using ', NCPU, ' cores.
-This is chunk ', TASK_ID,' of ', MAX_TASKS, '.')
-    fname = file.path('data', paste0('cca_perms-', SELECTION, '-chunk_', TASK_ID, '.rds'))
+This is chunk ', CHUNK_ID,' of ', MAX_TASKS, '.')
+    fname = file.path('data', paste0('cca_perms-', SELECTION, '-chunk_', CHUNK_ID, '.rds'))
     message('Output filename: ', fname)
-    permutations_i <- split(1:dim(permutations)[1], f = 1:MAX_TASKS)[[TASK_ID]]
+    permutations_i <- split(1:dim(permutations)[1], f = 1:MAX_TASKS)[[CHUNK_ID]]
     message('Running ', length(permutations_i), ' of ', dim(permutations)[1], ' permutations...')
   }
 }
@@ -32,11 +60,8 @@ if(file.exists(fname)){
 }
 ##---------Load packages and import data-------
 
-if (!require("PMA")) {install.packages("PMA"); require("PMA")}
-if (!require("readr")) {install.packages("readr"); require("readr")}
-
-behavioral_df_all <- data.frame(readr::read_csv('data/behavior_data.csv'))
-mri_df_all <- readRDS('data/rsfc_data.rds')
+behavioral_df_all <- data.frame(readr::read_csv('data/behavior_data_resid.csv'))
+mri_df_all <- readRDS('data/rsfc_data_resid.rds')
 sub_list_DT <- read.csv("subject_lists/Rest_DT-CCA_n118.csv")
 sub_list_MT <- read.csv("subject_lists/Rest_MT-CCA_n121.csv")
 
@@ -68,14 +93,27 @@ if(var(c(dim(permutations)[[2]], dim(mri_df)[[1]], dim(behavioral_df)[[1]])) > 0
 # correlation with any of the 17 IDS symptoms to preserve the same feature 
 # to subjects ratio (80% of 187 subjects).
 
-select_features_drysdale <- function(X, Y, n_selected_vars){
+select_features_drysdale <- function(X, Y, n_selected_vars = NULL){
+  if(is.null(n_selected_vars)){
+    total_n <- dim(X)[1]
+    n_selected_vars <- round(.80*total_n)
+  }
   correlations <- cor(Y, X, method = "spearman")
   correlations <- apply(correlations, 2, function(x){max(abs(x))})
   corr.threshold <- sort(correlations, decreasing = T)[n_selected_vars]
   selected.X <- correlations >= corr.threshold
   selected.X <- X[,selected.X]
+  return(selected.X)
 }
 
+select_features_drysdale2 <- function(X, Y, n_selected_vars = NULL){
+  if(is.null(n_selected_vars)){
+    total_p <- dim(X)[2]
+    n_selected_vars <- round(.20*total_p)
+  }
+  selected.X <- select_features_drysdale(X, Y, n_selected_vars = n_selected_vars)
+  return(selected.X)
+}
 
 ## Xia et al. (2018) method
 
@@ -85,18 +123,25 @@ select_features_drysdale <- function(X, Y, n_selected_vars){
 # absolute deviation, which is more robust against outliers than standard 
 # deviation.
 
-select_features_xia <- function(X, Y, n_selected_vars){
+select_features_xia <- function(X, Y, n_selected_vars = NULL){
+  if(is.null(n_selected_vars)){
+    total_p <- dim(X)[2]
+    round(.10*(total_p))
+  }
   mads <- apply(X, 2, mad)
   mads.threshold <- sort(mads, decreasing = T)[n_selected_vars]
   selected.X <- mads >= mads.threshold
   selected.X <- X[,selected.X]
+  return(selected.X)
 }
+
+selectfun <- eval(parse(text = paste0('select_features_', SELECTION)))
 
 # Select features + CCA function
 
-select_and_cca_fit <- function(X, Y, K, n_selected_vars, selection_function, return_cca_object = FALSE, nperms = 1000){
+select_and_cca_fit <- function(X, Y, K, selection_function, return_cca_object = FALSE, nperms = 1000){
   #select features
-  selected.X <- selection_function(X, Y, n_selected_vars)
+  selected.X <- selection_function(X, Y)
   #cca fit with best penalty
   penalties = seq(0, 1, length.out = 20) #This covers the whole range pretty well, though maybe overkill
   system.time(acca <- CCA.permute(Y, selected.X, typex = 'standard', typez = 'standard', 
@@ -115,45 +160,45 @@ select_and_cca_fit <- function(X, Y, K, n_selected_vars, selection_function, ret
   return(rez)
 }
 
-total_n <- dim(mri_df)[1]
 
-message('Creating cluster with ', NCPU, ' cores...')
-cl <- parallel::makePSOCKcluster(NCPU)
-message('Exporting data to cluster nodes...')
-parallel::clusterExport(cl = cl, 
-                        varlist = c('mri_df', 'behavioral_df', 'total_n',
-                                    'select_features_drysdale', 'permutations',
-                                    'select_and_cca_fit'))
-message('Running permutations...')
-system.time({
-  rez <- parallel::parLapply(
-    cl = cl, X = permutations_i, 
-    fun = function(i){
-      library(PMA)
-      perm <- permutations[i,]
-      cca_output_drysdale <- select_and_cca_fit(X = mri_df[perm, 2:ncol(mri_df)],
-                                                Y = behavioral_df[,2:ncol(behavioral_df)],
-                                                K = 10,
-                                                n_selected_vars = round(.80*total_n),
-                                                selection_function = select_features_drysdale)
-      return(cca_output_drysdale)
-    })
-})
-parallel::stopCluster(cl)
-
-saveRDS(rez, fname)
-
-# cca_output_xia <- select_and_cca_fit(X = mri_df[2:ncol(mri_df)], 
-#                                      Y = behavioral_df[2:ncol(behavioral_df)], 
-#                                      n_selected_vars = round(.10*(ncol(mri_df)-1)), # -1 to exclude the ID col; Y isn't used here
-#                                      selection_function = select_features_xia) 
-
-if(FALSE){
-  cca_rez <- select_and_cca_fit(X = mri_df[,2:ncol(mri_df)],
-                                Y = behavioral_df[,2:ncol(behavioral_df)],
-                                K = 10,
-                                n_selected_vars = round(.80*total_n),
-                                selection_function = select_features_drysdale,
-                                return_cca_object = TRUE)
-  saveRDS(cca_rez, file.path('data', paste0('cca-', SELECTION, '.rds')))
+if(!args$nopermute){
+  message('Creating cluster with ', NCPU, ' cores...')
+  cl <- parallel::makePSOCKcluster(NCPU)
+  message('Exporting data to cluster nodes...')
+  parallel::clusterExport(cl = cl, 
+                          varlist = c('mri_df', 'behavioral_df',
+                                      'permutations', lsf.str()))
+  message('Running permutations...')
+  system.time({
+    rez <- parallel::parLapply(
+      cl = cl, X = permutations_i, 
+      fun = function(i){
+        library(PMA)
+        perm <- permutations[i,]
+        cca_output <- select_and_cca_fit(X = mri_df[, 2:ncol(mri_df)],
+                                         Y = behavioral_df[,2:ncol(behavioral_df)],
+                                         K = 10, nperms = NPERMS,
+                                         selection_function = selectfun)
+        return(cca_output)
+      })
+  })
+  parallel::stopCluster(cl)
+  
+  message('Saving results...')
+  saveRDS(rez, fname)
+  message('Done permuting.')
+} else {
+  outfile <- file.path('data', paste0('cca-', SELECTION, '.rds'))
+  if(file.exists(outfile)){
+    stop(outfile, ' exists. Will not recompute.')
+  }
+  message('Estimating CCA on unpermuted data...')
+  system.time({cca_rez <- select_and_cca_fit(X = mri_df[,2:ncol(mri_df)],
+                                             Y = behavioral_df[,2:ncol(behavioral_df)],
+                                             K = 10, nperms = NPERMS,
+                                             selection_function = selectfun,
+                                             return_cca_object = TRUE)})
+  message('Saving result to ', outfile)
+  saveRDS(cca_rez, outfile)
+  message('Done.')
 }
